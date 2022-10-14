a76899226db9cc6004bcba17593ee185df774e13
Kamil Chwedura <kchwedura@gmail.com>
2022-10-14 13:37:34 +0100

    Setup basic Discord connection
    The bot can now connect to the Discord servers.

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+from operator import truediv
 import discord
+import json
 
+class DionysusClient(discord.Client):
+    # When the bot is ready, print a ready ping in console
+    async def on_ready(self):
+        print(f"Logged on as {self.user}!")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = DionysusClient(intents=intents)
+
+# Find a better way to store the token...
+with open("token.json") as f:
+    token = json.load(f)
+    client.run(token['TOKEN'])
